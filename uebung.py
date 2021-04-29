057db96dff63300692aeb53beb6b57a8aa97e1d9
Felicia Saar <sphew@posteo.de>
2021-04-29 11:14:52 +0200

    Vorlage Aufgabe 7.4 aus dem Skript

---
+++ uebung.py
@@ -0,0 +1,19 @@
+
+
+
+# Geheimnis = Schokokeks
+
+
+ergebnis = summe([2, 4.3, -1, 9])
+print(ergebnis) # Ausgabe: 14.3
+
+
+ergebnis = mittelwert([2, 4.3, -1, 9])
+print(ergebnis) # Ausgabe: 3.575
+
+
+paar = minmax([8, 3, 10.9, -3, 17, 0])
+print(paar) # Ausgabe: (-3, 17)
+
+
+print(spannweite[(2, 5, 4.3, 9])) # Ausgabe: 7
